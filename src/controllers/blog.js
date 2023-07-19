89ab800eec556849c17bc0ba99de69f019b7e740
fikar <trifikar.nutech@gmail.com>
2023-07-19 14:04:01 +0700

    chore: add api update blog post

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -47,7 +47,6 @@ exports.createBlogPost = (req, res, next) => {
     });
 
 }
-
 // Get All Blog Post
 exports.getAllBlogPost = (req, res, next) => {
     // find() to get all data in models blog
@@ -61,4 +60,80 @@ exports.getAllBlogPost = (req, res, next) => {
     .catch(err => {
         next(err);
     });
+}
+// Get Blog Post By Id
+exports.getBlogPostById = (req, res, next) => {
+    const postId = req.params.postId;
+    BlogPost.findById(postId)
+    .then(result => {
+        if(!result) {
+            const error = new Error('Blog Post tidak ditemukan');
+            error.errorStatus = 404;
+            throw error;
+        }
+        res.status(200).json({
+            message: 'Data Blog Post By Id Berhasil dipanggil',
+            data: result
+        })
+    })
+    .catch(err => {
+        next(err);
+    })
+}
+// Update Blog Post By Id
+exports.updateBlogPost = (req, res, next) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        // console.log('err :', errors);
+        const err = new Error('Input value tidak sesuai');
+        err.errorStatus = 400;
+        err.data = errors.array();
+        throw err;
+    }
+    // validasi for image file
+    if(!req.file) {
+        const err = new Error('Image harus Di Upload');
+        err.errorStatus = 422;
+        throw err;
+    }
+
+    const title = req.body.title;
+    const image = req.file.path; // image from muller
+    const body = req.body.body;
+    const postId = req.params.postId;
+
+    BlogPost.findById(postId)
+    .then(post => {
+        if(!post) {
+            const err = new Error('Blog Post tidak ditemukan');
+            err.errorStatus = 404;
+            throw err;
+        }
+
+        post.title = title;
+        post.body = body;
+        post.image = image;
+
+        return post.save();
+    })
+    .then(result => {
+        res.status(200).json({
+            message: 'Update Blog Post Berhasil',
+            data: result,
+        })
+    })
+    .catch(err => {
+        next(err);
+    })
+
+    const Posting = new BlogPost({
+        title: title,
+        body: body,
+        image: image,
+        author: {
+            uid: 1,
+            name: 'Tri Fikar',
+        }
+    })
 }
--- src/routes/blog.js
+++ src/routes/blog.js
@@ -5,12 +5,19 @@ const router = express.Router();
 
 const blogController = require('../controllers/blog');
 
-// [POST] : /v1/blog/post
+// [POST] : /v1/blog/post (create blog post)
 router.post('/post', [
     body('title').isLength({min: 5}).withMessage('input title minimum 5 karakter'),
     body('body').isLength({min: 5}).withMessage('input body minimum 5 karakter')],
     blogController.createBlogPost);
-// [GET] : /v1/blog/posts
+// [GET] : /v1/blog/posts (Get All blog)
 router.get('/posts', blogController.getAllBlogPost);
+// [GET] : /v1/blog/posts (Get blog by id)
+router.get('/post/:postId', blogController.getBlogPostById);
+// [PUT] : /v1/blog/posts (Update blog post by id)
+router.put('/post/:postId', [
+    body('title').isLength({min: 5}).withMessage('input title minimum 5 karakter'),
+    body('body').isLength({min: 5}).withMessage('input body minimum 5 karakter')],
+    blogController.updateBlogPost);
 
 module.exports = router;
